51c0572bf83dd262df65eb8b6b25cb98543f2f44
Efrén Fernández <efrenfernandez@Efren-Fernandez.local>
2014-06-20 01:33:16 +0200

    Starting main screen

---
+++ android-hackathon/src/com/icesabi/android_hackathon/ui/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.icesabi.android_hackathon.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.icesabi.android_hackathon.R;
+import com.icesabi.android_hackathon.ui.fragment.MainFragment;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new MainFragment())
+                    .commit();
+        }
+    }
+
+}
---
+++ android-hackathon/src/com/icesabi/android_hackathon/ui/fragment/MainFragment.java
@@ -0,0 +1,33 @@
+package com.icesabi.android_hackathon.ui.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.icesabi.android_hackathon.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+    
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+    	view.findViewById(R.id.button_find_buddy).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
+    }
+}
