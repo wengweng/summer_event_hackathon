c469a064e83a6abda3e5b47712523221a27697c0
Efrén Fernández <efrenfernandez@Efren-Fernandez.local>
2014-06-20 02:07:34 +0200

    Mock-up for image selection

--- android-hackathon/src/com/icesabi/android_hackathon/ui/fragment/BuddiesFragment.java
+++ android-hackathon/src/com/icesabi/android_hackathon/ui/fragment/BuddiesFragment.java
@@ -1,33 +1,113 @@
 package com.icesabi.android_hackathon.ui.fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.icesabi.android_hackathon.R;
+
+import domain.Buddy;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class BuddiesFragment extends Fragment {
 
-    @Override
+    private GridView grid;
+    private BuddyAdapter adapter;
+
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_buddies, container, false);
         return rootView;
     }
     
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
-    	view.findViewById(R.id.button_find_buddy).setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
+    	 grid = (GridView) view.findViewById(R.id.grid_buddies);
+    	 adapter = new BuddyAdapter(getActivity());
+    	 List<Buddy> list = new ArrayList<Buddy>();
+    	 for (int i = 0;i < 80 ;i++) {
+    		 list.add(new Buddy("Henk", "henk"));
+    	 }
+    	 adapter.setList(list);
+    	 grid.setAdapter(adapter);
+    	 
+    }
+    
+    public class BuddyAdapter extends BaseAdapter {
+    	
+    	
+    	private final Context mContext;
+    	private List<Buddy> mList;
+    	
+    	public BuddyAdapter(Context context) {
+    		mContext = context;
+    	}
+
+		public List<Buddy> getList() {
+			return mList;
+		}
+
+		public void setList(List<Buddy> mList) {
+			this.mList = mList;
+		}
+
+		@Override
+		public int getCount() {
+			if (mList == null) {
+				return 0;
 			}
-		});
+			else {
+				return mList.size();
+			}
+		}
+
+		@Override
+		public Buddy getItem(int position) {
+			if (mList == null){
+				return null;
+			}
+			else {
+				return mList.get(position);
+			}
+		}
+
+		@Override
+		public long getItemId(int position) {
+			if (mList == null){
+				return -1;
+			}
+			else {
+				return mList.get(position).hashCode();
+			}
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = LayoutInflater.from(mContext).inflate(R.layout.buddy_item, null);
+			}
+			
+			Buddy buddy = getItem(position);
+			ImageView image = ((ImageView) convertView.findViewById(R.id.buddy_picture));
+			image.setImageDrawable(mContext.getResources().getDrawable(android.R.drawable.ic_menu_rotate));
+			
+			TextView name =  ((TextView) convertView.findViewById(R.id.buddy_name));
+			name.setText(buddy.getName());
+			return convertView;
+		}
+    	
     }
 }
--- android-hackathon/src/com/icesabi/android_hackathon/ui/fragment/MainFragment.java
+++ android-hackathon/src/com/icesabi/android_hackathon/ui/fragment/MainFragment.java
@@ -1,12 +1,14 @@
 package com.icesabi.android_hackathon.ui.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.icesabi.android_hackathon.R;
+import com.icesabi.android_hackathon.ui.activity.BuddiesActivity;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -26,7 +28,8 @@ public class MainFragment extends Fragment {
 			
 			@Override
 			public void onClick(View v) {
-				
+				Intent intent = new Intent(getActivity(),BuddiesActivity.class);
+				startActivity(intent);
 			}
 		});
     }
